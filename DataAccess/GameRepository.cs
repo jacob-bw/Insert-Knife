ab493bdea89a5baf92028e08d7c59340d48a3932
Jacob <wittenberg.jacob@gmail.com>
2020-07-18 13:49:28 -0500

    found order by NewID() for sql random selection from answer population tables

--- DataAccess/GameRepository.cs
+++ DataAccess/GameRepository.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Insert_Knife.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using Dapper;
 
 namespace Insert_Knife.DataAccess
 {
     public class GameRepository
     {
+        private string ConnectionString;
+
+        public GameRepository(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("InsertKnife");
+        }
+
+        public Game StartNewGame(
+            int gameId,
+            int answerWeaponId,
+            int answerSuspectId,
+            int answerRoomId)
+        {
+            var sql = @"";
+
+            var parameters = new
+            {
+                GameId = gameId,
+                AnswerWeaponId = answerWeaponId,
+                AnswerSuspectId = answerSuspectId,
+                AnswerRoomId = answerRoomId
+            };
+
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var result = db.QueryFirstOrDefault<Game>(sql, parameters);
+                return result;
+            }
+        }
     }
+
 }
--- DataAccess/GuessRepository.cs
+++ DataAccess/GuessRepository.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Insert_Knife.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace Insert_Knife.DataAccess
 {
     public class GuessRepository
     {
+        private string ConnectionString;
+
+        public GuessRepository(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("InsertKnife");
+        }
+
+        public Guess 
     }
 }
